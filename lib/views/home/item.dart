34a9e7a31229aecfb931e1571ee0651ece706717
FelipeQueFez <felipequefez@gmail.com>
2020-03-15 19:57:46 -0300

    card with image

--- lib/views/home/item.dart
+++ lib/views/home/item.dart
@@ -1,16 +1,31 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class Item extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 130,
-      child: Card(
-        elevation: 10,
-        child: Row(
-          children: <Widget>[Text('Hello')],
-        ),
+    return Card(
+      semanticContainer: true,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Column(
+        children: <Widget>[
+          CachedNetworkImage(
+            fit: BoxFit.fill,
+            imageUrl: "https://placeimg.com/640/480/any",
+            placeholder: (context, url) => CircularProgressIndicator(),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            child: Text('test'),
+          )
+        ],
       ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 5,
+      margin: EdgeInsets.all(10),
     );
   }
 }
